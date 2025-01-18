7c5e9b63c15ea56ac6feada3a969ffaafaaf86f8
itcube22 <104897682+itcube22@users.noreply.github.com>
2025-01-18 12:15:53 +0700

    cube

--- Project/Assets/Game Assets/Scripts/Interactor.cs
+++ Project/Assets/Game Assets/Scripts/Interactor.cs
@@ -1,26 +1,85 @@
 using UnityEngine;
 using UnityEngine.Events;
+using UnityEngine.InputSystem;
 
 public class Interactor : MonoBehaviour
 {
     [SerializeField] private InteractButton interactButton;
-
+    [SerializeField] private Transform uiButtonSpawnParent;
     public UnityEvent onEnteredZone, onInteracted, onLeftZone;
 
     private enum InteractButton { AButton,BButton }
 
-    private void OnTriggerEnter(Collider other)
+    private GameObject spawnedButton;
+
+    private InputActionReference aInput, bInput;
+
+    private void OnEnable()
     {
         
     }
 
-    private void OnTriggerStay(Collider other)
+    private void BButtonPressed(InputAction.CallbackContext obj)
+    {
+        onInteracted?.Invoke();
+    }
+
+    private void AButtonPressed(InputAction.CallbackContext obj)
+    {
+        onInteracted?.Invoke();
+        Test("venom");
+    }
+
+    private void OnDisable()
     {
         
+    }
+
+    public void Test(string textt)
+    {
+        Debug.Log(textt);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            
+            onEnteredZone?.Invoke();
+            switch (interactButton)
+            {
+                case InteractButton.AButton:
+                    spawnedButton = Instantiate(UIUXManager.instance.buttonA, uiButtonSpawnParent != null ? uiButtonSpawnParent : transform);
+                    aInput = UIUXManager.instance.aInput;
+                    aInput.action.performed += AButtonPressed;
+                    Debug.Log(aInput);
+                    break;
+
+                case InteractButton.BButton:
+                    spawnedButton = Instantiate(UIUXManager.instance.buttonB, uiButtonSpawnParent != null ? uiButtonSpawnParent : transform);
+                    bInput = UIUXManager.instance.bInput;
+                    bInput.action.performed += BButtonPressed;
+                    break;
+            }
+        }
     }
 
     private void OnTriggerExit(Collider other)
     {
-        
+        if(spawnedButton!=null)
+            Destroy(spawnedButton);
+
+        onLeftZone?.Invoke();
+
+        switch(interactButton)
+        {
+            case InteractButton.AButton:
+                aInput = null;
+                break;
+
+            case InteractButton.BButton:
+                bInput = null;
+                break;
+        }
     }
 }
